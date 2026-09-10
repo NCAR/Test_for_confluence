a627266b3be3649de49d8a0e8357759f988f5dc3
KalyaniChilakapatiA <kalyaniadmin@ucar.edu>
2026-09-10 12:46:07 -0600

    Update publish_confluece.py

--- publish_confluece.py
+++ publish_confluece.py
@@ -1,1 +1,9 @@
+print("Python script is running")
 
+#with open("wiki/Home.md", "r") as file: means: open the Home.md file inside the wiki folder in read mode. "r" means read.
+
+with open("wiki/Home.md", "r") as file:  
+  content = file.read()                 #reads everything inside Home.md and stores it in content.
+
+print("Wiki Home page content:")
+print(content)
